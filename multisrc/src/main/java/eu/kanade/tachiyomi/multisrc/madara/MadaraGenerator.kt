695343002bbb92f7aa83d72bbc9e50b70eb747fb
Vetle Ledaal <vetle.ledaal@gmail.com>
2023-12-16 10:44:18 +0100

    Update domain for Shinigami

--- multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/madara/MadaraGenerator.kt
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/madara/MadaraGenerator.kt
@@ -434,7 +434,7 @@ class MadaraGenerator : ThemeSourceGenerator {
         SingleLang("Shayami", "https://shayami.com", "es"),
         SingleLang("Shiba Manga", "https://shibamanga.com", "en"),
         SingleLang("Shield Manga", "https://shieldmanga.io", "en", overrideVersionCode = 3),
-        SingleLang("Shinigami", "https://shinigami.sh", "id", overrideVersionCode = 7),
+        SingleLang("Shinigami", "https://shinigami.moe", "id", overrideVersionCode = 8),
         SingleLang("Shooting Star Scans", "https://shootingstarscans.com", "en"),
         SingleLang("ShoujoHearts", "https://shoujohearts.com", "en", overrideVersionCode = 2),
         SingleLang("Sinensis Scan", "https://sinensisscan.net", "pt-BR", pkgName = "sinensis", overrideVersionCode = 6),
