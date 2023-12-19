9e603298f1745d3890355ca7e97f07b5f75be0f2
Vetle Ledaal <vetle.ledaal@gmail.com>
2023-12-19 16:59:31 +0100

    amend! Update domain for Ikifeng
    Update domain for Ikifeng -> Lector Online

--- multisrc/overrides/madara/ikifeng/src/Ikifeng.kt
+++ multisrc/overrides/madara/ikifeng/src/Ikifeng.kt
@@ -6,7 +6,10 @@ import okhttp3.Response
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class Ikifeng : Madara("Ikifeng", "https://lectorunm.life", "es", SimpleDateFormat("dd/MM/yyyy", Locale("es"))) {
+class Ikifeng : Madara("Lector Online", "https://lectorunm.life", "es", SimpleDateFormat("dd/MM/yyyy", Locale("es"))) {
+    // Ikifeng (es) -> Lector Online (es)
+    override val id = 2087311173049672570
+
     override val useNewChapterEndpoint = true
 
     override fun searchPage(page: Int): String = if (page == 1) "" else "page/$page/"
